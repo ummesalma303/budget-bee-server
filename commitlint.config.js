f229485b1e9f0aaa552f44a49e97ad4376d2db9c
ummesalma303 <ummesalma333999@gmail.com>
2026-02-08 08:00:04 +0600

    fix: Format files with prettier to pass CI

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "chore", "style", "refactor", "ci", "test", "revert", "perf", "vercel"]],
+    "subject-case": [2, "always", "sentence-case"],
+  },
+};
