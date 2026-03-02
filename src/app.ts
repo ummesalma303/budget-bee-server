00297e73cf4d0550622f5cec63fbff75be739e12
ummesalma303 <ummesalma333999@gmail.com>
2026-03-03 02:18:21 +0600

    feat: App.ts, logger.ts, tsconfig.json and package.json

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,35 @@ import globalErrorHandler from '#middlewares/globalErrorHandler.js'
 import { middleware } from '#middlewares/middleware.js'
 import notFound from '#middlewares/notFound.js'
 import router from '#modules/expense/expense.routes.js'
+import logger from '#utils/logger.js'
 import cors from 'cors'
 import express, { Application } from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
 import path from 'path'
 
 const app: Application = express()
 
-// Middleware
+// Middleware setup
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(express.static(path.join(process.cwd(), '../', 'public')))
 app.get('/', middleware)
 
-// router
+// morgan setup to use winston for logging HTTP requests
+app.use(
+    morgan('combined', {
+        stream: {
+            write: (message) => logger.info(message.trim())
+        }
+    })
+)
+
+// router setup
 app.use('/api/v1', router)
 
 // error handler
-
 app.use(notFound)
 app.use(globalErrorHandler)
 export default app
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,25 +1,40 @@
 import config from '#config/config.js'
 import { EApplicationEnvironment } from '#constant/application.js'
-// import { blue, cyan, green, magenta, red, yellow } from 'colorette'
+import { blue, cyan, green, magenta, red, yellow } from 'colorette'
 import path from 'path'
-// import util from 'util'
 import winston from 'winston'
+import 'winston-daily-rotate-file'
 
 const { createLogger, format, transports } = winston
-const { colorize, combine, errors, json, printf, timestamp } = format
+const { combine, errors, json, printf, timestamp } = format
 import { TransformableInfo } from 'logform'
+
+/* ------------------------------------ colorful terminal ----------------------------------- */
+const colorLevel = (level: string): string => {
+    const strLevel = level.toUpperCase()
+    switch (strLevel) {
+        case 'DEBUG':
+            return blue(strLevel)
+        case 'ERROR':
+            return red(strLevel)
+        case 'INFO':
+            return green(strLevel)
+        case 'WARN':
+            return yellow(strLevel)
+        default:
+            return cyan(strLevel)
+    }
+}
 
 /* ---------------------- Console Format (Dev Friendly) --------------------- */
 const devFormat = printf((info: TransformableInfo) => {
     const { level, message, stack, timestamp, ...meta } = info
 
-    const metaData = Object.keys(meta).length > 0 ? `\nMeta: ${JSON.stringify(meta, null, 2)}` : ''
-
+    const metaData = Object.keys(meta).length > 0 ? `\n${magenta('Meta')}: ${JSON.stringify(meta, null, 2)}` : ''
     const logMessage = typeof stack === 'string' ? stack : typeof message === 'string' ? message : ''
-
     const time = typeof timestamp === 'string' ? timestamp : ''
 
-    return `${time} [${level}]: ${logMessage}${metaData}`
+    return `${blue(time)} [${colorLevel(level)}]: ${logMessage}${metaData}`
 })
 
 /* -------------------- File Format (Production Friendly) ------------------- */
@@ -30,18 +45,22 @@ const logger = createLogger({
     format: errors({ stack: true }),
     level: config.ENV === EApplicationEnvironment.DEVELOPMENT ? 'debug' : 'info',
     transports: [
-        new transports.File({
-            filename: path.join(process.cwd(), 'logs', `${config.ENV ?? 'development'}.log`),
-            format: prodFormat
+        new transports.DailyRotateFile({
+            datePattern: 'YYYY-MM-DD',
+            filename: path.join(process.cwd(), 'logs', '%DATE%-combined.log'),
+            format: prodFormat,
+            maxFiles: '14d',
+            maxSize: '20m',
+            zippedArchive: true
         })
     ]
 })
 
-/* ------------------------------------ colorful terminal ----------------------------------- */
+/* ------------------------------------ colorful transport ----------------------------------- */
 if (config.ENV === EApplicationEnvironment.DEVELOPMENT) {
     logger.add(
         new transports.Console({
-            format: combine(colorize(), timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), devFormat)
+            format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), devFormat)
         })
     )
 }
